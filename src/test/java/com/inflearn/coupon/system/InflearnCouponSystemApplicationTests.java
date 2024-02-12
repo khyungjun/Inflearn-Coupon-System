4849fd2a2e533ba1b83dd0b38d71dd6b413ac982
khyungjun <kpos7503@naver.com>
2024-02-12 23:58:09 +0900

    update project

---
+++ src/test/java/com/inflearn/coupon/system/InflearnCouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inflearn.coupon.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnCouponSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
